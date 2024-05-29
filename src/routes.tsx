e104b073ab00acc6541f1246fc904527f762ee11
Gabriel Albano <albanogabriel33@gmail.com>
2024-05-29 17:58:54 -0300

    almost finished

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,15 +3,17 @@ import { NotFound } from "./Pages/404";
 import { Data } from "./Pages/Data/data";
 import { AppLayout } from "./layout/app-layout";
 import { Home } from "./Pages/Home/home";
+import { Edu } from "./Pages/Edu/edu";
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <AppLayout/>,
+    element: <AppLayout />,
     errorElement: <NotFound />,
     children: [
       { path: '/', element: <Home /> },
       { path: '/data', element: <Data /> },
+      { path: '/edu', element: <Edu /> },
     ],
   },
 ])
